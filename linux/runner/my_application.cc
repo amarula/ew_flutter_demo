f9c8c4fa47f63abb301a08b5acd95052fdd7bf79
Andrea Ricchi <andrea.ricchi@amarulasolutions.com>
2025-11-06 17:12:42 +0100

    Update window name and resolution
    Signed-off-by: Andrea Ricchi <andrea.ricchi@amarulasolutions.com>

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -46,14 +46,14 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "test_ew_demo");
+    gtk_header_bar_set_title(header_bar, "ew_flutter_demo");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "test_ew_demo");
+    gtk_window_set_title(window, "ew_flutter_demo");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 854, 480);
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
