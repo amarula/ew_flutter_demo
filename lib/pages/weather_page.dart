b1f125bd9ac01ab9594c183f3c8d078a7aff2a7e
Peter Janicka <peterj@amarulasolutions.com>
2026-01-23 08:47:21 +0100

    Add weather_page
    Added page for display of the Weather Foreceast info.
    
    Signed-off-by: Peter Janicka <peterj@amarulasolutions.com>

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,29 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import 'package:ew_2026_flutter_demo/components/weather_page_appbar.dart';
+import 'package:ew_2026_flutter_demo/widgets/weather_forecast_widget.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: WeatherPageAppbar(),
+      backgroundColor: Color(0xFF222630),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [WeatherForecastWidget()],
+        ),
+      ),
+    );
+  }
+}
