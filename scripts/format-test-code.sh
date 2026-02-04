b7346e93cf9abc47cc5a92b347f14fab63eabdec
Andrea Ricchi <andrea.ricchi@amarulasolutions.com>
2026-02-04 15:53:47 +0100

    format-test-code.sh: update script to format FFI libraries
    Signed-off-by: Andrea Ricchi <andrea.ricchi@amarulasolutions.com>

--- scripts/format-test-code.sh
+++ scripts/format-test-code.sh
@@ -5,7 +5,24 @@ SCRIPT_PATH=$(dirname $FULL_PATH)
 
 cd ${SCRIPT_PATH}/..
 
-dart format lib test
+echo -e "----------------------------------------------------------------------------------\n"
+
+echo -e "Formatting libcppconnman_adapter"
+cd libcppconnman_adapter
+cmake-format -i CMakeLists.txt
+clang-format -i libcppconnman_adapter.cpp
+cd ..
+
+echo -e "Formatting libsensors_ffi"
+cd libsensors_ffi
+cmake-format -i CMakeLists.txt
+clang-format -i libsensors_ffi.cpp
+cd ..
+
+
+echo -e "\n----------------------------------------------------------------------------------\n"
+
+dart format lib
 
 echo -e "\n----------------------------------------------------------------------------------\n"
 
@@ -14,7 +31,3 @@ dart run import_sorter:main
 echo -e "\n----------------------------------------------------------------------------------\n"
 
 flutter analyze
-
-echo -e "\n----------------------------------------------------------------------------------\n"
-
-flutter test
