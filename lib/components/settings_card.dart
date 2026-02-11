a173ec27243e8db09e4dfeb888b4f12e0b1ec382
Peter Janicka <peterj@amarulasolutions.com>
2026-02-11 17:51:42 +0100

    Add SettingsCard
    Added component for settings, which displays the "current" value and
    allows to start edit on touch.
    
    Signed-off-by: Peter Janicka <peterj@amarulasolutions.com>

---
+++ lib/components/settings_card.dart
@@ -0,0 +1,86 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SettingsCard extends StatelessWidget {
+  const SettingsCard({
+    required this.title,
+    required this.value,
+    required this.onPressed,
+    this.iconAssetsPath,
+    this.iconData,
+    super.key,
+  });
+
+  final String title;
+  final String value;
+  final VoidCallback onPressed;
+  final String? iconAssetsPath;
+  final IconData? iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 250,
+      decoration: BoxDecoration(
+        color: const Color(0xFF373e4e),
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(24),
+        child: InkWell(
+          onTap: onPressed,
+          borderRadius: BorderRadius.circular(24),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    if (iconAssetsPath != null)
+                      SvgPicture.asset(
+                        iconAssetsPath!,
+                        width: 20,
+                        height: 20,
+                      )
+                    else if (iconData != null)
+                      Icon(
+                        iconData,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+
+                    const SizedBox(width: 8),
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+
+                Text(
+                  value,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
