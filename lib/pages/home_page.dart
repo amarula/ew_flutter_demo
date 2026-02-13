d0581cd3e264744327fcf0e228d7dc344d95100b
Peter Janicka <peterj@amarulasolutions.com>
2026-02-13 13:15:17 +0100

    Add footer area to HomePage
    Added display of sensor values and power switch to the HomePage.
    
    Signed-off-by: Peter Janicka <peterj@amarulasolutions.com>

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,12 @@
 // Flutter imports:
+import 'package:ew_2026_flutter_demo/components/sensor_value_card.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:lottie/lottie.dart';
 
 // Project imports:
+import 'package:ew_2026_flutter_demo/components/power_switch.dart';
 import 'package:ew_2026_flutter_demo/components/temp_change_button.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,44 +18,88 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool isHeating = false;
+  bool _turnedOn = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: const HomePageAppbar(),
       backgroundColor: const Color(0xFF222630),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
           children: [
-            TempChangeButton(
-              icon: const Icon(Icons.expand_less),
-              onPressed: () {
-                isHeating = true;
-                setState(() {});
-              },
+            SizedBox(
+              height: 330,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TempChangeButton(
+                    icon: const Icon(Icons.expand_less),
+                    onPressed: () {
+                      isHeating = true;
+                      setState(() {});
+                    },
+                  ),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Lottie.asset(
+                        'assets/lottie/${isHeating ? 'heating' : 'cooling'}_ring.json',
+                      ),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text('24°', style: TextStyle(fontSize: 72)),
+                          Text(isHeating ? 'Heat' : 'Cool'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  TempChangeButton(
+                    icon: const Icon(Icons.expand_more),
+                    onPressed: () {
+                      isHeating = false;
+                      setState(() {});
+                    },
+                  ),
+                ],
+              ),
             ),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Lottie.asset(
-                  'assets/lottie/${isHeating ? 'heating' : 'cooling'}_ring.json',
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text('24°', style: TextStyle(fontSize: 72)),
-                    Text(isHeating ? 'Heat' : 'Cool'),
-                  ],
-                ),
-              ],
-            ),
-            TempChangeButton(
-              icon: const Icon(Icons.expand_more),
-              onPressed: () {
-                isHeating = false;
-                setState(() {});
-              },
+            SizedBox(
+              height: 150,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(width: 16),
+                  const SensorValueCard(
+                    title: 'Temperature',
+                    value: '24',
+                    icon: Icon(Icons.thermostat, size: 32),
+                    unit: '°C',
+                  ),
+                  const SensorValueCard(
+                    title: 'Humidity',
+                    value: '60',
+                    unit: '%',
+                    icon: Icon(
+                      Icons.water_drop,
+                      size: 32,
+                    ),
+                  ),
+                  const SensorValueCard(
+                    title: 'Pressure',
+                    value: '1013',
+                    icon: Icon(Icons.air, size: 32),
+                    unit: 'hPa',
+                  ),
+                  PowerSwitch(
+                    turnedOn: _turnedOn,
+                    onChanged: (value) {
+                      _turnedOn = value;
+                      setState(() {});
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
